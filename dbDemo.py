0809d42e346ff3bc5795aa63a7533d71ada7f638
Aykchen <aliceykchen01@gmail.com>
2023-02-11 23:25:52 -0500

    Remove Platform as Entity

--- dbDemo.py
+++ dbDemo.py
@@ -4,12 +4,11 @@ from dbSetup import connection, cursor
 # cursor.execute("CREATE TABLE Location(locationID VARCHAR, city VARCHAR, placeType VARCHAR, completeLocation VARCHAR, country VARCHAR, countryCode VARCHAR);")
 # cursor.execute("CREATE TABLE UserTwitter(userID INT, description TEXT, username VARCHAR, followerCount INT, created VARCHAR);")
 # cursor.execute("CREATE TABLE Tweet(tweetID INT, text TEXT, language VARCHAR, dateTime VARCHAR);")
-# cursor.execute("CREATE TABLE Platform(platformID INT, platformName VARCHAR);")
 # cursor.execute("CREATE TABLE TweetMetrics(tweetID INT, tweetMetricID INT, replyCount INT, retweetCount INT, favoriteCount INT);")
 # cursor.execute("CREATE TABLE Hashtag(tweetID INT, hashtagID INT, text TEXT);")
 
 # Creation of relationships
-# cursor.execute("CREATE TABLE uses(userID INT, platformID INT);")
+# cursor.execute("CREATE TABLE tweets(userID INT, tweetID INT);")
 # cursor.execute("CREATE TABLE lives(userID INT, locationID INT);")
 # cursor.execute("CREATE TABLE locatedFrom(locationID INT, tweetID INT);")
 # cursor.execute("CREATE TABLE has(tweetMetricID INT, tweetID INT);")
@@ -19,9 +18,13 @@ from dbSetup import connection, cursor
 # cursor.execute("ALTER TABLE Location ADD PRIMARY KEY(locationID);")
 # cursor.execute("ALTER TABLE UserTwitter ADD PRIMARY KEY(userID);")
 # cursor.execute("ALTER TABLE Tweet ADD PRIMARY KEY(tweetID);")
-# cursor.execute("ALTER TABLE Platform ADD PRIMARY KEY(platformID);")
 # cursor.execute("ALTER TABLE TweetMetrics ADD PRIMARY KEY(tweetID, tweetMetricID);")
 # cursor.execute("ALTER TABLE Hashtag ADD PRIMARY KEY(tweetID, hashtagID);")
+# cursor.execute("ALTER TABLE tweets ADD PRIMARY KEY(userID INT, tweetID INT);")
+# cursor.execute("ALTER TABLE lives ADD PRIMARY KEY(userID INT, locationID INT);")
+# cursor.execute("ALTER TABLE locatedFrom ADD PRIMARY KEY(locationID INT, tweetID INT);")
+# cursor.execute("ALTER TABLE has ADD PRIMARY KEY(tweetMetricID INT, tweetID INT);")
+# cursor.execute("ALTER TABLE contains ADD PRIMARY KEY(hashtagID INT, tweetID INT);")
 
 # Defining foreign key
 # cursor.execute("ALTER TABLE Hashtag ADD FOREIGN KEY (tweetID) REFERENCES Tweet (tweetID);")
